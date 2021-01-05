1769fc1a94795a1c3d6ac9b7d47ff7c75114c10e
amiraElmergawy <49119157+amiraElmergawy@users.noreply.github.com>
2021-01-05 09:20:36 +0200

    Fixed deletion process of tasks

--- firstTask/js/script.js
+++ firstTask/js/script.js
@@ -3,13 +3,13 @@ let taskKeys = ['title', 'type', 'description'],
         {
             title: 'first title',
             type: 't1',
-            description: 'test',
+            description: 'first description',
             status: false
         },
         {
             title: 'second title',
             type: 't3',
-            description: 'test2',
+            description: 'second description',
             status: false
         },
     ],
@@ -35,12 +35,12 @@ showTasks = function (flag = false) { // flag will be false at the first time ca
         if (flag) {
             // append only the last task
             div = document.createElement('div');
-            addElement('span', todoList.length-1, div, 'className', 'd-none');
+            addElement('span', todoList.length - 1, div, 'className', 'd-none');
             taskKeys.forEach(key => {
                 addElement('span', todoList[todoList.length - 1][key], div);
             });
-            addElement('i','',div,'className','fas fa-trash btn text-white');
-            addElement('i','',div,'className','fas fa-pencil-alt btn text-white');
+            addElement('i', '', div, 'className', 'fas fa-trash btn text-white');
+            addElement('i', '', div, 'className', 'fas fa-pencil-alt btn text-white');
             div.className = 'task';
             dataSec.appendChild(div);
         } else {
@@ -48,12 +48,12 @@ showTasks = function (flag = false) { // flag will be false at the first time ca
             dataSec.innerHTML = '';
             todoList.forEach((task, i) => {
                 div = document.createElement('div');
-                addElement('span',i , div, 'className', 'd-none');
+                addElement('span', i, div, 'className', 'd-none');
                 taskKeys.forEach(key => {
                     addElement('span', task[key], div);
                 });
-                addElement('i','',div,'className','fas fa-trash btn text-white');
-                addElement('i','',div,'className','fas fa-pencil-alt btn text-white');
+                addElement('i', '', div, 'className', 'fas fa-trash btn text-white');
+                addElement('i', '', div, 'className', 'fas fa-pencil-alt btn text-white');
                 div.className = 'task';
                 dataSec.appendChild(div);
             });
@@ -86,12 +86,57 @@ form.addEventListener('submit', function (e) {
 showTasks();
 
 //deletion process
-document.querySelector('.fa-trash').addEventListener('click', function (e) {
-    let index = +e.target.parentElement.firstChild.innerHTML;
+// console.log(allDeletionNodes);
+// document.querySelectorAll('.fa-trash').forEach(item => {
+//     item.addEventListener('click', function (e) {
+//         let deletionDiv = e.target.parentElement;
+//         let index = +deletionDiv.firstChild.innerHTML;
+//         // console.log(deletionDiv.parentElement);
+//         deletionDiv.parentElement.removeChild(deletionDiv);
+//         // console.log(deletionDiv.parentElement);
+//         // allDeletionNodes.splice(index, 1);
+//         todoList.splice(index, 1);
+//         window.localStorage.setItem('todoList', JSON.stringify(todoList));
+//         console.log(document.querySelectorAll('.fa-trash'));
+//         showTasks(false);
+//     })
+// })
+
+
+// let allDeletionNodes = document.querySelectorAll('.fa-trash'); // save all divs that can be deleted
+// dataSec.addEventListener('click', function (e) {
+//     console.log('el event 7sl');
+//     console.log(dataSec.contains);
+//     console.log(dataSec.innerHTML);
+//     console.log(e.target.parentElement)
+//     allDeletionNodes.forEach((div, index) => {
+//         console.log('mn dakhl el 7adth elaul');
+//         if (div == e.target.parentElement) {
+//             console.log('mn dakhl el 7adth');
+//             dataSec.removeChild(div);
+//             todoList.splice(index, 1);
+//             window.localStorage.setItem('todoList', JSON.stringify(todoList));
+//             showTasks(false);
+//         }
+//     })
+// })
+
+// document.getElementsByClassName('.fa-trash').addEventListener('click', function (e) {
+//     let index = +e.target.parentElement.firstChild.innerHTML;
+//     todoList.splice(index, 1);
+//     window.localStorage.setItem('todoList', JSON.stringify(todoList));
+//     showTasks(false);
+// })
+
+dataSec.addEventListener('click', function (e) {
+    let deletionDiv = e.target.parentElement;
+    let index = +deletionDiv.firstChild.innerHTML; // access first hidden span that contain its id
+    dataSec.removeChild(deletionDiv);
     todoList.splice(index, 1);
     window.localStorage.setItem('todoList', JSON.stringify(todoList));
     showTasks(false);
 })
+
 
 //editting process
 document.querySelector('.fa-pencil-alt').addEventListener('click', function (e) {
